70b4f0701b483cca9594e58a2f7b0d76bb22c34c
Alexander Zwierzynski <alexanderzwierzynski@Alexanders-MacBook-Pro-2.local>
2023-09-01 16:28:28 +0200

    added uploadStream method

--- index.js
+++ index.js
@@ -2,67 +2,73 @@ const Readable = require('stream').Readable;
 const ftpClient = require("basic-ftp");
 
 module.exports = {
-    init(config) {
-        const getConnection = async () => {
-            const client = new ftpClient.Client();
-            await client.access({
-                host: config.host,
-                user: config.user,
-                password: config.password,
-                secure: config.secure,
-            });
-            return client;
-        };
+  
+  init(config) {
+    const getConnection = async () => {
+      const client = new ftpClient.Client();
+      await client.access({
+        host: config.host,
+        user: config.user,
+        password: config.password,
+        secure: config.secure,
+      });
+      return client;
+    };
 
-        const uploadStream = async (file) => {
-            const path = `${config.path}/${file.hash}${file.ext}`;
-            const client = await getConnection();
+    const uploadStream = async (file) => {
+      const path = `${config.path}/${file.hash}${file.ext}`;
+      const client = await getConnection();
 
-            try {
-                const source = new Readable();
-                source._read = () => {}; // _read is required but you can noop it
-                source.push(file.buffer);
-                source.push(null);
-                await client.uploadFrom(source, path);
+      try {
+        const source = new Readable();
+        source._read = () => {}; // _read is required but you can noop it
+        source.push(file.buffer);
+        source.push(null);
+        await client.uploadFrom(source, path);
 
-            } catch (error) {
-                throw error;
-            } finally {
-                await client.close();
-            }
-        };
+      } catch (error) {
+        throw error;
+      } finally {
+        await client.close();
+      }
+    };
 
-        const deleteFile = async (file) => {
-            const path = `${config.path}/${file.hash}${file.ext}`;
-            const client = await getConnection();
+    const deleteFile = async (file) => {
+      const path = `${config.path}/${file.hash}${file.ext}`;
+      const client = await getConnection();
 
-            try {
-                await client.remove(path);
-            } catch (error) {
-                throw error;
-            } finally {
-                await client.close();
-            }
-        };
+      try {
+        await client.remove(path);
+      } catch (error) {
+        throw error;
+      } finally {
+        await client.close();
+      }
+    };
 
-        return {
-            async upload(file) {
-                await uploadStream(file);
-                file.url = `${config.baseUrl}/${file.hash}${file.ext}`;
-            },
+    return {
+
+      async uploadStream(file) {
+        await upload(file);
+      },
+
+      async upload(file) {
+        await uploadStream(file);
+        file.url = `${config.baseUrl}/${file.hash}${file.ext}`;
+      },
 
 
-            delete(file) {
-                return new Promise((resolve, reject) => {
-                    deleteFile(file)
-                        .then(() => {
-                               resolve();
-                        })
-                        .catch((error) => {
-                            reject(error);
-                        });
-                });
-            },
-        };
-    },
+      delete(file) {
+        return new Promise((resolve, reject) => {
+          deleteFile(file)
+          .then(() => {
+            resolve();
+          })
+          .catch((error) => {
+            reject(error);
+          });
+        });
+      },
+    };
+  },
 };
